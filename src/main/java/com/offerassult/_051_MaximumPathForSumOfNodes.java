84ee5a419f1edc32c46895387070a086b808b078
vivi <xidian.wangwei@gmail.com>
2022-12-06 17:32:40 +0800

    daily and offerassult

--- src/main/java/com/offerassult/_051_MaximumPathForSumOfNodes.java
+++ src/main/java/com/offerassult/_051_MaximumPathForSumOfNodes.java
@@ -1,27 +1,30 @@
-package com.hot100;
+package com.offerassult;
 
 import com.common.TreeNode;
 
 /**
  * @author wangwei
- * 2022/4/17 18:22
+ * @date 2022/12/6 11:53
+ * @description: _051_MaximumPathForSumOfNodes
  *
- * 124. 二叉树中的最大路径和
+ * 剑指 Offer II 051. 节点之和最大的路径
  * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
  *
  * 路径和 是路径中各节点值的总和。
  *
- * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 给定一个二叉树的根节点 root ，返回其 最大路径和，即所有路径上节点值之和的最大值。
  *
  *
  *
  * 示例 1：
  *
  *
+ *
  * 输入：root = [1,2,3]
  * 输出：6
  * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
  * 示例 2：
+ *
  *
  *
  * 输入：root = [-10,9,20,null,null,15,7]
@@ -33,8 +36,11 @@ import com.common.TreeNode;
  *
  * 树中节点数目范围是 [1, 3 * 104]
  * -1000 <= Node.val <= 1000
+ *
+ *
+ * 注意：本题与主站 124 题相同： https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/
  */
-public class _124_MaximumPathSumOfBinaryTree {
+public class _051_MaximumPathForSumOfNodes {
 
     /**
      * 递归 + 后序遍历
