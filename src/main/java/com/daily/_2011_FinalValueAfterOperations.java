7ac2ff0b27c95458bd73a10078b03fa8ad7f05dc
vivi <xidian.wangwei@gmail.com>
2022-12-23 13:54:18 +0800

    daily

---
+++ src/main/java/com/daily/_2011_FinalValueAfterOperations.java
@@ -0,0 +1,80 @@
+package com.daily;
+
+/**
+ * @author wangwei
+ * @date 2022/12/23 13:51
+ * @description: _2011_FinalValueAfterOperations
+ *
+ * 2011. 执行操作后的变量值
+ * 存在一种仅支持 4 种操作和 1 个变量 X 的编程语言：
+ *
+ * ++X 和 X++ 使变量 X 的值 加 1
+ * --X 和 X-- 使变量 X 的值 减 1
+ * 最初，X 的值是 0
+ *
+ * 给你一个字符串数组 operations ，这是由操作组成的一个列表，返回执行所有操作后， X 的 最终值 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：operations = ["--X","X++","X++"]
+ * 输出：1
+ * 解释：操作按下述步骤执行：
+ * 最初，X = 0
+ * --X：X 减 1 ，X =  0 - 1 = -1
+ * X++：X 加 1 ，X = -1 + 1 =  0
+ * X++：X 加 1 ，X =  0 + 1 =  1
+ * 示例 2：
+ *
+ * 输入：operations = ["++X","++X","X++"]
+ * 输出：3
+ * 解释：操作按下述步骤执行：
+ * 最初，X = 0
+ * ++X：X 加 1 ，X = 0 + 1 = 1
+ * ++X：X 加 1 ，X = 1 + 1 = 2
+ * X++：X 加 1 ，X = 2 + 1 = 3
+ * 示例 3：
+ *
+ * 输入：operations = ["X++","++X","--X","X--"]
+ * 输出：0
+ * 解释：操作按下述步骤执行：
+ * 最初，X = 0
+ * X++：X 加 1 ，X = 0 + 1 = 1
+ * ++X：X 加 1 ，X = 1 + 1 = 2
+ * --X：X 减 1 ，X = 2 - 1 = 1
+ * X--：X 减 1 ，X = 1 - 1 = 0
+ *
+ *
+ * 提示：
+ *
+ * 1 <= operations.length <= 100
+ * operations[i] 将会是 "++X"、"X++"、"--X" 或 "X--"
+ * 通过次数29,276提交次数32,909
+ */
+public class _2011_FinalValueAfterOperations {
+
+    /**
+     * 简单模拟，
+     * ans初始值为0
+     * 遍历operation数组，如果是“x++”或“++x”，就让ans自增，否则自减
+     * 返回ans
+     * @param operations
+     * @return
+     */
+    public int finalValueAfterOperations(String[] operations) {
+        int ans = 0;
+        // 遍历操作
+        for (String operation : operations) {
+            // 自增操作
+            if (operation.charAt(1) == '+') {
+                ans++;
+            // 自减操作
+            } else {
+                ans--;
+            }
+        }
+        // 返回
+        return ans;
+    }
+}
